a424e5cdf2019a6ec9bcf83a51909f68265346e9
Valdegg <vae11@hi.is>
2019-03-23 13:05:20 +0000

    Add files via upload

--- labtwin_data_normalization.py
+++ labtwin_data_normalization.py
@@ -12,7 +12,19 @@ import inflect
 import string 
 
 
+
+result_name = "papers_aggregated.txt"
+# 'result_name' is the name of the file which will contain the aggregate of the files
+# intially it should be an empty file
+ 
+path = "C:/Users/Valdi/Documents/papers/"
+# 'path' is the folder which contains the papers and an initially empty text file whose name is the value of 'result_name'
+
+result = open(path + result_name, "a")
+# result is an object for the result file to be written into
+
 inflector = inflect.engine()
+# inflector is an object for changing numbers into spoken form, used by numberToSpoken()
 
 
 
@@ -32,47 +44,54 @@ def  removePunctuation(strengur):
 	return ''.join(word.strip(string.punctuation) for word in strengur)
 	
 
-
-
-def numberToSpoken(strengur ): 
-	""" Use: spoken = numberToSpoken(number)
-	Before: 'strengur' is a string that contains numbers e.g. "it costs 1000 dollars"
-	After: 'spoken' is 'strengur' with numbers in spoken form, e.g. "it costs one thousand dollars" """
-	return inflector.number_to_words(strengur)
-	#return ''.join(inflector.number_to_words(word) for word in strengur if  
-
-print(removePunctuation("# After: 'spoken' is 'number' in... spoken form, e.g. 'twelve'"))
-print(numberToSpoken("asldka"))
-
-
-s = "hg19 reference genome using novoalign software version 20714  picard software version 167  and the genome analysis toolkit gatk  27 variant discovery genotype calling and annota"
-s2 = "hg19 reference genome using novoalign software version 20714.32  picard software version 167  and the genome analysis toolkit gatk  27 variant discovery genotype calling and annota"
-l = []
-for t in s.split():
-    try:
-        l.append(float(t))
-    except ValueError:
-        pass
-print(l)
-# þetta nær 20714 og 27 en bætir við .0
-# þarf að gera greinarmun á ints og floats 
-	# ef það er punktur í orðinu (ekki í endann) og orðið samanstendur annars af tölum þá float 
-# þarf fyrst að finna orðin, allt sem hefur bil í kringum sig  \b\w\b eitthvað
-	# nei bara Ekki bil 
-	
 def hasNumbers(inputString):
+	""" Use: b = hasnumbers(inputString)
+	Before: 'inputString' is a string 
+	After: b is True iff 'inputString' contains a digit"""
 	return any(char.isdigit() for char in inputString)
+
+
+def isFloat(inputString):
+	""" Use: b = hasnumbers(inputString)
+	Before: 'inputString' is a word string (no whitespaces)
+	After: b is True iff 'inputString' is a rational number 
+	i.e. is a string of only numbers and one dot in the middle, like 0.1  and isn't an integer like 12"""
+		
 	
+	return inputString.replace('.','',1).isdigit() and not inputString.isdigit()
+	
+def isACT1231(inputString):
+	""" Use: b = hasnumbers(inputString)
+	Before: 'inputString' is a word string (no whitespaces)
+	After: b is True iff 'inputString' is a letter sequence that ends with a number sequence
+	e.g. K562"""
+		# split into two 	
+		# not enough time for this feature at the moment 
+	
+	return	bool(re.match('^([a-zA-Z]+[0-9]+)$', inputString))
+	
+def is1231ACT():
+	""" same as isACT1231 but with number that ends with character sequence """ 
+	return 0
 
-result_name = "papers_aggregated.txt"
-# 'result_name' is the name of file containing the aggregate of the files
+def numberToSpoken(strengur): 
+	""" Use: spoken = numberToSpoken(number)
+	Before: 'strengur' is a string that may contain numbers e.g. "it costs 1000 dollars and is worth 1.5 hours"
+	After: 'spoken' is 'strengur' with numbers in spoken form, e.g. "it costs one thousand dollars and is worth one point five hours" """
+	words = strengur.split()
 
- 
-path = "C:/Users/Valdi/Documents/papers/"
-# 'path' is the folder which contains the papers and an initially empty text file whose name is the value of 'result_name'
+	words2 = [inflector.number_to_words(word) if isFloat(word) or word.isdigit() else  word  for word in words]
+	#words3 = [inflector.number_to_words(word, group = 1) if isACT1231(word) else  word  for word in words2]
+		# later time feature 
+	if words2 != []:
+		words3 = functools.reduce( lambda x,y: x + " " + y, words2 ) 
+	else: 
+		words3 = ""
+	return words3
 
-result = open(path + result_name, "a")
-# result is an object for the result file to be written into
+
+
+	
 
 
 
@@ -84,7 +103,7 @@ for file_name in sorted(os.listdir(path)):
 	# Paragraphs from the html files are separated by new-lines. 
 	# html files are separated by lines with *** Original paper file name: FILENAME ***
 	
-	if(file_name[-4:] == "html" and i < 0):
+	if(file_name[-4:] == "html"):
 		
 		html = codecs.open(path + file_name, 'r').read()  
 		# 'html' is the html file in 'file_name'
@@ -94,26 +113,28 @@ for file_name in sorted(os.listdir(path)):
 		
 		paragraphs = [p.get_text() for p in soup.findAll('p')]	
 		# all the paragraphs from paper with name 'file_name' are in 'paragraphs'
-
-		text = functools.reduce( lambda x,y: newLineCat(x,y), paragraphs ) 
+		
+		words_no_numbers = [numberToSpoken(paragraph) for paragraph in paragraphs]
+		# words_no_numbers is the paragraphs with number words like 1.5 and 333 changed to spoken form. 
+		
+		text = functools.reduce( lambda x,y: newLineCat(x,y), words_no_numbers ) 
 		# 'text' is a single string with all paragraphs with an empty line in between 
 		
+
 		text2 = unicodedata.normalize('NFKD',text).encode('ascii','ignore').decode().lower()
-		# 'text2' is text transformed such that it's a string in ASCII, in lower case.		
+		# 'text2' is text transformed such that it's a string in ASCII (not bytes), in lower case.		
 
 		text3  = re.sub(r'http\S+', '', text2)
 		# text3 doesn't contain links 
 		
-		#text3 = numberToSpoken(
-	# ath þarf að vera gert áður en punctuation er tekið svo 1.5 verði one point five 
-		
+
 		text4 = removePunctuation(text3)		
 		
-		# 'text5' is a string with the contents of the paper from 'file_name', in ASCII, in lower case and without punctuation and with numbers in spoken form 
+		# 'text4' is a string with the contents of the paper from 'file_name', in ASCII, in lower case and without punctuation and with numbers in spoken form 
 		
 		result.write("\n" + "*** Original paper file name: " + file_name  + " ***\n\n") 
-		result.write(text5 +"\n") 
-		# the contents of 'text5' have been written to the result file 
+		result.write(text4 +"\n") 
+		# the contents of 'text4' have been written to the result file 
 		
 		i = i +1 
 				
@@ -123,13 +144,16 @@ for file_name in sorted(os.listdir(path)):
  
 
 result.close()
- 
-# testa minimum requirements
- 
-# vista aggregate í skjal
 
-		#bla = re.sub(r'(\b\w+\b)', "5", s)
-			# lambda x: removePunctuation(x.group())		
-		#text3 = re.sub(r'[^\s]+', lambda word: ''.join(e for e in word if e.isalnum()), text2)
-		# every sequence of not-whitespaces (i.e. words) which contain only alphanumerics (non-punctuation)
-
+# some tests I used :
+tests = False
+if(tests):
+	# print(bool(re.match('^([a-zA-Z]+[0-9]+)$', 'hasAlphanum123')))
+	# s = "ara12311"
+	# print(isFloat("12.5"))
+	# print(isFloat("5"))
+	# print("15".isdigit())		
+	# print(removePunctuation("# After: 'spoken' is 'number' in... spoken form, e.g. 'twelve'"))
+	# print(inflector.number_to_words("12.5"))
+	# print(inflector.number_to_words("125"))
+	 print(numberToSpoken("ACS300 costs 1000 dollars and is worth 1.5 hours of time"))
